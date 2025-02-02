763573445a4f5d90b71d7e0a9c026e9eb51f27b7
jeffrey-clements <freeclements@gmail.com>
2025-02-02 06:35:21 -0600

    deploy

--- server/server.js
+++ server/server.js
@@ -7,18 +7,18 @@ const connectDB = require("./config/db");
 
 const app = express();
 
-// Connect to database
+
 connectDB();
 
-// Middleware
+
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 app.use(morgan("dev"));
 
-// Routes
 app.get("/", (req, res) => res.send("API is running"));
 
-// Start server
+
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
